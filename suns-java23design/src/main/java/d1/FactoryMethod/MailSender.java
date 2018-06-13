7e5d347cd94769f28aa7169d4ff2220a2388ac29
suntion <suntion@yeah.net>
2018-06-13 22:13:06 +0800

    增加jdk版本

--- suns-java23design/src/main/java/d1/FactoryMethod/MailSender.java
+++ suns-java23design/src/main/java/d1/FactoryMethod/MailSender.java
@@ -6,10 +6,9 @@ package d1.FactoryMethod;
 /**
  * TODO MailSender
  * @author suns sontion@yeah.net
- * @since 2017年3月15日下午3:13:47
+ * @since 2017骞�3鏈�15鏃ヤ笅鍗�3:13:47
  */
 public class MailSender implements Sender {  
-    @Override  
     public void Send() {  
         System.out.println("this is mailsender!");  
     }
